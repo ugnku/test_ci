2c01032b78f8882cc21bb0f1f5fc02586132d135
ugnku <104075008+ugnku@users.noreply.github.com>
2022-05-17 15:34:19 +0300

    Fix PaymentPageTest::GetUrl() (#2)

--- tests/PaymentPageTest.php
+++ tests/PaymentPageTest.php
@@ -23,7 +23,7 @@ class PaymentPageTest extends TestCase
         $url = $paymentPage->getUrl($payment);
 
         self::assertEquals(
-            'https://paymentpage.ecommpay.com/payment?project_id=100&interface_type=%7B%22id%22%3A23%7D'
+            'https://paymentpage.ecommpay.com/payment/?project_id=100&interface_type=%7B%22id%22%3A23%7D'
             . '&payment_id=test+payment+id&payment_description=B%26W&signature=97JFQpAyJ4HPfGVedJh0M1MqQDOFt%2FM'
             . 'Cbdh8VrsT7DdRyTBDAF2mvUOsDANx1ZPfbvZg0%2BVUbF43xJnq0jEeLA%3D%3D',
             $url
